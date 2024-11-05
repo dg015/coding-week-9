22256e578a0fe66eaf1e9d92ec1c86e8a264fa8d
dg015 <bruno.barbosa8@yahoo.com>
2024-11-05 14:37:35 -0500

    Cannon exercise complete

--- Barbosa week 9/Assets/Scripts/CannonballController.cs
+++ Barbosa week 9/Assets/Scripts/CannonballController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class CannonballController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private ScoreboardController scoreboard;
     void Start()
     {
+        scoreboard = GameObject.Find("Scoreboard").GetComponent<ScoreboardController>();
         Destroy(gameObject, 3);
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -13,6 +15,11 @@ public class CannonballController : MonoBehaviour
         if (collision.gameObject.CompareTag("cannon ball"))
         {
             Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("target zone"))
+        {
+            
+            scoreboard.Score += 1;
         }
     }
 
